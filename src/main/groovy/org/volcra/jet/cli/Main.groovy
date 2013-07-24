22158dd50368a1b80eca54447850aadeb18721fd
Emanuelle Garduno <emanuelle.garduno@gmail.com>
2013-07-23 21:58:07 -0400

    Version 0.1.1, adding license, groovydoc and default directory

--- src/main/groovy/org/volcra/jet/cli/Main.groovy
+++ src/main/groovy/org/volcra/jet/cli/Main.groovy
@@ -1,23 +1,56 @@
+/*
+ * Copyright 2013 Volcra
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.volcra.jet.cli
 
 import org.volcra.jet.server.WebServer
 
+/**
+ * Main class.
+ */
 class Main {
+    /**
+     * Parse the command line arguments.
+     *
+     * <p>No arguments means use current directory as document root.
+     *
+     * @param args command line arguments.
+     */
     static void main(String... args) {
-        def options = CommandLine.instance.parse(args)
-        def arguments = options.arguments()
+        def options = CommandLine.instance.parse args
 
-        if (arguments.isEmpty() || options.h) help()
+        if (options.h) help()
         else run options
     }
 
+    /**
+     * Print help information.
+     */
     private static void help() {
         CommandLine.instance.usage()
     }
 
+    /**
+     * Start the Web Server.
+     *
+     * @param options command line options
+     */
     private static void run(options) {
-        def server = new WebServer(directory: options.arguments()[0])
+        def server = new WebServer()
 
+        if (options.arguments()) server.directory = options.arguments().head()
         if (options.p) server.port = options.p as Integer
 
         server.start()
--- src/main/groovy/org/volcra/jet/server/WebServer.groovy
+++ src/main/groovy/org/volcra/jet/server/WebServer.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Volcra
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.volcra.jet.server
 
 import org.eclipse.jetty.server.Server
@@ -5,27 +20,51 @@ import org.eclipse.jetty.server.handler.DefaultHandler
 import org.eclipse.jetty.server.handler.HandlerList
 import org.eclipse.jetty.server.handler.ResourceHandler
 
+/**
+ * Jet WebServer.
+ */
 class WebServer {
-    private final int port
+    /**
+     * Running port
+     */
+    int port
 
-    private final String directory
+    /**
+     * Base directory to start serving files.
+     */
+    String directory
 
+    /**
+     * Jetty Server.
+     */
     private Server server
 
+    /**
+     * Constructor.
+     *
+     * @param port default is 8008
+     * @param directory default is current directory
+     */
     WebServer(int port = 8080, String directory = '.') {
         this.port = port
         this.directory = directory
     }
 
+    /**
+     * Start the web server.
+     */
     void start() {
         def resourceHandler = new ResourceHandler(directoriesListed: true,
-            welcomeFiles: ['index.html'], resourceBase: directory)
+                welcomeFiles: ['index.html'], resourceBase: directory)
 
         server = new Server(port)
         server.handler = new HandlerList(handlers: [resourceHandler, new DefaultHandler()])
         server.start()
     }
 
+    /**
+     * Stop the webserver.
+     */
     void stop() {
         server.stop()
         server.join()
